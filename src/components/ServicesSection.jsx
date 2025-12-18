dcd40a42a24bfb765deef9cca27ed60755bbf154
azddoumaryam <azddoumaryam@gmail.com>
2025-12-18 21:11:54 +0100

    initial commit of complex moovup project

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -67,6 +67,7 @@ export default function ServicesSection() {
 </div>
 
 
+
       {/* === Services Grid === */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {services.map((service, idx) => (
